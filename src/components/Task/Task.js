94c27fa03539236f70d1520a6eda1949c0c9ae9f
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-11-01 14:11:46 -0300

    Añadida propiedad ubicación

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,12 +1,12 @@
 import { STATES_LABEL } from "../consts";
 
 export class Task {
-  constructor(title, description, expirationDate) {
+  constructor(title, description, dueDate) {
     this.title = title;
     this.description = description;
-    this.expirationDate = new Date(expirationDate);
+    this.dueDate = new Date(dueDate);
     this._id = "UUID";
     this.state = STATES_LABEL.NEW;
-    this.createdAtDate = new Date();
+    this.location = "location";
   }
 }
