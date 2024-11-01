59153dac9ddc802d4dec2829a0f779f5e7c6d240
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-11-01 11:00:42 -0300

    State icon con atributo color para mostrar estado actual

--- src/components/StateIcon/StateIconElement.js
+++ src/components/StateIcon/StateIconElement.js
@@ -1,13 +1,28 @@
-const STATES_COLOR = {
-  NEW: "#656F7D",
-  IN_PROGRESS: "#40A6E6",
-  COMPLETED: "#F9BE33",
-};
+import { STATES_COLOR } from "../consts";
 
 export class StateIconElement extends HTMLElement {
+  static get observedAttributes() {
+    return ["color"];
+  }
+
   constructor() {
     super();
 
     this.color = this.getAttribute("color") ?? STATES_COLOR.NEW;
+
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = `<span style="vertical-align: middle; display: inline-block; width: 15px; height: 15px; border-radius: 100%; background-color: ${this.color};"></span>`;
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === "color") {
+      this.updateColor(newValue);
+    }
+  }
+
+  updateColor(color) {
+    this.color = color;
   }
 }
+
+customElements.define("state-icon-element", StateIconElement);
