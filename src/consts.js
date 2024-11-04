f8c3571ebc699d1d169dcd56144f772dfc96b529
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-11-04 13:00:18 -0300

    Constantes modificadas

--- src/consts.js
+++ src/consts.js
@@ -1,26 +1,20 @@
+// Para evitar que se modifiquen las propiedades del objeto
+export const STATES = Object.freeze({
+  NEW: "NEW",
+  IN_PROGRESS: "IN_PROGRESS",
+  COMPLETED: "COMPLETED",
+});
+
+// Para mostrar visualmente el nombre cada key
 export const STATES_LABEL = {
   NEW: "Nueva",
   IN_PROGRESS: "En progreso",
   COMPLETED: "Completada",
 };
 
+// Colores para cada estado
 export const STATES_COLOR = {
   NEW: "#656F7D",
   IN_PROGRESS: "#40A6E6",
   COMPLETED: "#F9BE33",
 };
-
-export const STATES = [
-  {
-    label: STATES_LABEL.NEW,
-    color: STATES_COLOR.NEW,
-  },
-  {
-    label: STATES_LABEL.IN_PROGRESS,
-    color: STATES_COLOR.IN_PROGRESS,
-  },
-  {
-    label: STATES_LABEL.COMPLETED,
-    color: STATES_COLOR.COMPLETED,
-  },
-];
