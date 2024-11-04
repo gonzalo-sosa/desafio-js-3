ee9c145371a3179a8f53b2de8a5c25076d8e5bfe
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-11-04 10:38:01 -0300

    Clase para manejar almacenamiento

---
+++ src/modules/Storage.js
@@ -0,0 +1,9 @@
+export class Storage {
+  save() {
+    throw new Error("Se debe de implementar el método 'save'");
+  }
+
+  load() {
+    throw new Error("Se debe de implementar el método 'save'");
+  }
+}
