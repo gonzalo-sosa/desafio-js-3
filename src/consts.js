1d98d09925d934384a3c04818f9ecd0a3955017a
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-11-04 10:35:29 -0300

    Constantes

---
+++ src/consts.js
@@ -0,0 +1,26 @@
+export const STATES_LABEL = {
+  NEW: "Nueva",
+  IN_PROGRESS: "En progreso",
+  COMPLETED: "Completada",
+};
+
+export const STATES_COLOR = {
+  NEW: "#656F7D",
+  IN_PROGRESS: "#40A6E6",
+  COMPLETED: "#F9BE33",
+};
+
+export const STATES = [
+  {
+    label: STATES_LABEL.NEW,
+    color: STATES_COLOR.NEW,
+  },
+  {
+    label: STATES_LABEL.IN_PROGRESS,
+    color: STATES_COLOR.IN_PROGRESS,
+  },
+  {
+    label: STATES_LABEL.COMPLETED,
+    color: STATES_COLOR.COMPLETED,
+  },
+];
