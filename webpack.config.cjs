89b983b291449eebb284007bc3cc51b80f58ac34
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-11-04 10:35:18 -0300

    Config para crypto

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,6 +2,8 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const path = require("path");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
   entry: "./src/index.js",
@@ -53,13 +55,29 @@ module.exports = {
       },
     ],
   },
+  resolve: {
+    fallback: {
+      vm: false,
+      crypto: require.resolve("crypto-browserify"),
+      stream: require.resolve("stream-browserify"),
+      process: require.resolve("process/browser"),
+    },
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
+      favicon: "./src/assets/favicon.ico",
     }),
     new MiniCssExtractPlugin({
-      filename: "./styles/[name].css",
+      filename: "[name].css",
       chunkFilename: "[id].css",
     }),
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+    }),
+    new webpack.NormalModuleReplacementPlugin(/node:crypto/, (resource) => {
+      resource.request = resource.request.replace(/^node:/, "");
+    }),
+    new NodePolyfillPlugin(),
   ],
 };
