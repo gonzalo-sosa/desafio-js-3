91bb4ce5e73e36db9b2be761e7440cebe0747e70
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-11-04 15:06:02 -0300

    Modificado valor por defecto

--- src/modules/LocalStorage.js
+++ src/modules/LocalStorage.js
@@ -1,14 +1,19 @@
 import { Storage } from "./storage";
 
 export class LocalStorage extends Storage {
+  /**
+   * @param {string} key
+   * @param {string} value
+   * @returns {void}
+   */
   static save(key, value) {
     localStorage.setItem(key, value);
   }
+  /**
+   * @param {string} key
+   * @param {string} defaultValue
+   */
   static load(key, defaultValue) {
-    return JSON.parse(localStorage.getItem(key) || `${defaultValue}`);
-  }
-
-  static clear() {
-    localStorage.clear();
+    return JSON.parse(localStorage.getItem(key) || defaultValue);
   }
 }
