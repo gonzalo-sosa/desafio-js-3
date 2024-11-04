3a3c2ae2eea3c59d0571c1927afe07d96b082790
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-11-04 10:36:07 -0300

    Métodos agregados

---
+++ src/modules/TaskList.js
@@ -0,0 +1,41 @@
+import { Task } from "./Task";
+
+export class TaskList {
+  constructor(tasks) {
+    this._list = tasks ?? [];
+  }
+
+  addTask(task) {
+    if (!(task instanceof Task)) throw new Error("No es tarea válida.");
+
+    this._list.push(task);
+  }
+
+  removeTask(toRemove) {
+    const index = this._list.findIndex((task) => task._id == toRemove._id);
+
+    if (index === -1) throw new Error("Tarea inexistente.");
+
+    this._list.splice(index, 1);
+  }
+
+  getTasks() {
+    return this._list;
+  }
+
+  getTaskById(id) {
+    return this._list.find((task) => task._id === id);
+  }
+
+  clear() {
+    this._list = [];
+  }
+
+  toJSON() {
+    return this._list.map((task) => task.toJSON());
+  }
+
+  toString() {
+    return JSON.stringify(this._list);
+  }
+}
