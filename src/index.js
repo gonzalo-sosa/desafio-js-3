0d3523e3cffe565e3c6bb0a0454ebff9e8bd8e15
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-11-05 14:53:22 -0300

    Crear marcadores en mapa de cada tarea

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import "./styles/main.css";
 
 import { Task, TaskList, LocalStorage } from "./modules/index";
 import { TaskElement } from "./components/index";
-import { STATES, TASKS } from "./consts";
+import { STATES, STATES_COLOR, TASKS } from "./consts";
+import { getPosition } from "./services/location";
 
 // Traer las tareas del local storage
 const newTaskList = new TaskList(LocalStorage.load(TASKS.NEW, "[]"));
@@ -42,12 +43,15 @@ if (completedTaskList.getTasks().length > 0)
   });
 
 // Manejo del formulario para agregar tareas
-$addTaskForm.addEventListener("submit", (event) => {
+$addTaskForm.addEventListener("submit", async (event) => {
   event.preventDefault();
 
   const { title, description, dueDate } = getFormData(event);
+  const [latitude, longitude] = await getPosition().then(
+    (position) => position
+  );
 
-  const task = new Task(title, description, dueDate);
+  const task = new Task(title, description, dueDate, latitude, longitude);
 
   newTaskList.addTask(task);
 
@@ -68,7 +72,7 @@ $addTask.addEventListener("click", function () {
  *
  */
 function addTaskElementToListElement(
-  { id, title, description, dueDate, state, location },
+  { id, title, description, dueDate, state, latitude, longitude },
   list
 ) {
   const taskElement = `<task-element
@@ -77,7 +81,8 @@ function addTaskElementToListElement(
       title="${title}" 
       due-date="${new Date(dueDate).toLocaleDateString()}"
       state="${state}"
-      location="${location}" 
+      latitude="${latitude}" 
+      longitude="${longitude}"
     >
     ${description}
     </task-element>`;
@@ -186,4 +191,54 @@ function getFormData(event) {
     description,
     dueDate,
   };
+}
+
+// Mapa
+const $map = document.getElementById("map");
+$map.style.height = "300px";
+
+const latitude = -34.61315;
+const longitude = -58.67723;
+
+const map = L.map($map, {
+  center: [latitude, longitude],
+  zoom: 10,
+  minZoom: 10,
+});
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+// TODO: para cada tarea crear un marker del color de la tarea
+// TODO: cuando se elimina tarea también eliminar los markers
+
+const markersForNew = newTaskList
+  .getTasks()
+  .map((task) => createMarkerInMapForTask(task));
+
+const markersForInProgress = inProgressTaskList
+  .getTasks()
+  .map((task) => createMarkerInMapForTask(task));
+
+const markersForCompleted = completedTaskList
+  .getTasks()
+  .map((task) => createMarkerInMapForTask(task));
+
+function createMarkerInMapForTask(task) {
+  const circle = L.circle(L.latLng(task.latitude, task.longitude), {
+    color: STATES_COLOR[task.state],
+    fillColor: STATES_COLOR[task.state],
+    fillOpacity: 0.5,
+    radius: 5,
+  }).addTo(map);
+
+  const marker = L.marker(L.latLng(task.latitude, task.longitude), {
+    alt: task.title,
+    title: task.title,
+  }).addTo(map);
+
+  return [circle, marker];
 }
