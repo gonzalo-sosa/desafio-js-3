22ee57770460774524e4ad3ad23cb6d03831cc25
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-11-06 15:40:40 -0300

    Exportar componentes

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,7 @@
 export * from "./PopoverElement";
 export * from "./StateIconElement";
 export * from "./TaskElement";
+export * from "./NotificationElement";
+export * from "./TabManager";
+export * from "./TabContent";
+export * from "./TabButton";
