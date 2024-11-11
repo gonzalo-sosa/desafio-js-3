81476c108f4c653dca3f8e0f4e0624adf8aced73
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-11-11 15:06:38 -0300

    Worker para notificaciones al momento de crear una tarea

---
+++ src/services/notification-worker.js
@@ -0,0 +1,10 @@
+// Crear notificación y enviarlas a todos los conectados al web socket
+// Si no hay conectados enviarla a si mismo
+
+onmessage = function (e) {
+  const { title, dueDate } = e.data;
+
+  new Notification("Lista de tareas", {
+    body: `Nueva tarea ${title} con expiración el ${dueDate.toLocaleString()}`,
+  });
+};
