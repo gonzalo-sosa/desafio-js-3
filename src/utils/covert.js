a80edc4c05779e4c590372b8209c1c70a6d26620
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-11-12 13:19:06 -0300

    Convertir blob a base 64

---
+++ src/utils/covert.js
@@ -0,0 +1,21 @@
+export function saveBlobToLocalStorage(blob, key) {
+  const reader = new FileReader();
+  reader.onload = function () {
+    localStorage.setItem(key, reader.result);
+  };
+  reader.readAsDataURL(blob);
+}
+
+export function loadBlobFromLocalStorage(key, type) {
+  const base64Data = localStorage.getItem(key);
+  if (!base64Data) {
+    return null;
+  }
+
+  const byteCh = atob(base64Data.split(",")[1]);
+  const byteN = new Uint8Array(byteCh.length);
+  for (const i in byteCh) {
+    byteN[i] = byteCh.charCodeAt(i);
+  }
+  return new Blob([byteN], { type });
+}
