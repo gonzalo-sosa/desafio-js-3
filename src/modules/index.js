a261d2f1141b7837dbd29a9c2fd31b20390a075c
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-11-14 17:21:47 -0300

    Exportar archivos desde index

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,3 +1,6 @@
 export * from './LocalStorage';
 export * from './Task';
 export * from './TaskList';
+export * from './TabContentEditor';
+export * from './CanvasEditor';
+export * from './DetailsEditor';
